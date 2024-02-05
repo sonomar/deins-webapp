b857b03ca40300bbbbb13bab4a7ec7af14e326b7
ankitkumar9018 <ankitkumar.9018@gmail.com>
2024-02-05 11:38:40 +0100

    settings page syntax fixed

--- app/(pages)/settings/page.tsx
+++ app/(pages)/settings/page.tsx
@@ -1,5 +1,5 @@
 import UserAccount from '@/app/components/web3Components/UserAccount'
-import SignMessage from '@/app/components/web3Components/signMessage'
+import SignMessage from '@/app/components/web3Components/SignMessage'
 import React from 'react'
 
 export default function Settings() {
