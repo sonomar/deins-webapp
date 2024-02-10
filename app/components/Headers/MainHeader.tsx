488a9c199f81b2e74ca6e871634fc1538dc2cb66
ankitkumar9018 <ankitkumar.9018@gmail.com>
2024-02-10 02:41:01 +0100

    styling fix

--- app/components/Headers/MainHeader.tsx
+++ app/components/Headers/MainHeader.tsx
@@ -2,14 +2,21 @@
 import Link from "next/link";
 import { ThemeContext } from "../../context/ThemeContext";
 import { useContext } from "react";
-import Image from "next/image";
 
 export default function Header() {
   const { theme, changeTheme } = useContext(ThemeContext);
   return (
     <div className="navbar  justify-between bg-base-100">
       <Link className="btn btn-ghost text-xl fill-current" href="/">
-        <Image src="/mandala.svg" alt="Company logo" width="50" height="50" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 50 50"
+          width="50"
+          height="50"
+        >
+          {" "}
+          <path d="M30,43h17l-4-4l-0.027-28.496l3.891-4.49H34.845l-9.24,22.73L15.497,6H3l4,5.091v24.195L1,43h14l-6-7.714V13.75L22,43 l-0.002,0.014L34,13.545V39L30,43z M10.911,41H5.089L8,37.258L10.911,41z M21.922,37.899L10.828,12.938L8.633,8h5.564l9.581,21.556 l0.805,1.81L21.922,37.899z M35.414,40.414L36,39.828V39V13.545v-5.06l0.191-0.47h6.293l-1.022,1.18l-0.489,0.565l0.001,0.747 L41,39.002l0.001,0.827l0.585,0.585L42.172,41h-7.343L35.414,40.414z" />
+        </svg>
       </Link>
       <div className="flex justify-end gap-5">
         {theme === "dark" ? (
