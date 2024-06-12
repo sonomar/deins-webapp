d01f50fef1fcf540f7b44d22e72a2930daee2b1e
sonomar <lawsonmarlowe@gmail.com>
2024-06-12 10:49:23 +0200

    update table to have multiple columns

--- schemas/public/Names.ts
+++ schemas/public/Names.ts
@@ -3,13 +3,18 @@
 
 import { type ColumnType, type Selectable, type Insertable, type Updateable } from 'kysely';
 
+/** Identifier type for public.names */
+export type NamesId = number & { __brand: 'NamesId' };
+
 /** Represents the table public.names */
 export default interface NamesTable {
-  id: ColumnType<number | null, number | null, number | null>;
+  id: ColumnType<NamesId, NamesId, NamesId>;
 
   firstname: ColumnType<string | null, string | null, string | null>;
 
   lastname: ColumnType<string | null, string | null, string | null>;
+
+  email: ColumnType<string | null, string | null, string | null>;
 }
 
 export type Names = Selectable<NamesTable>;
